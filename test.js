6a3f374b8e02fc33c7843edd1f734e93bdf335cd
yuksel <yukselbey35@gmail.com>
2021-02-21 07:19:27 -0700

    Example of mongoDB

--- test.js
+++ test.js
@@ -8,9 +8,41 @@ mongoose.connect('mongodb://127.0.0.1/nodejs_blog_test_db', {
   useUnifiedTopology: true
 })
 
+Post.findByIdAndUpdate('603166b9f9d0f904483e82af', {
+  title: 'my 1st title'
+}, (error, post) =>{
+  console.log(error, post)
+})
+
+/* 
+//to find by id 
+Post.findById('603166b9f9d0f904483e82af', (error, post)=>{
+  console.log(error, post)
+})
+
+ */
+
+/* 
+//to see all data
+Post.find({ }, (error, post)=>{
+  console.log(error, post)
+}) 
+
+ *//* 
+//finding just one data
+Post.find({
+  title: '"my first title'
+}, (error, post)=>{
+  console.log(error, post)
+}) */
+
+/* 
+//creating first data
 Post.create({
-    title: 'my first title',
+    title: 'my second post of the title',
     content: 'content of the post'
 }, (error, post)=>{
     console.log(error, post)
 })
+
+ */
